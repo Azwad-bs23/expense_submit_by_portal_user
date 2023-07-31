a1ad8f4f0b92625035449bb395b0e72d61eca348
Azwad-bs23 <Afid.Azwad@brainstation-23.com>
2023-07-31 13:06:15 +0600

    [EA-5632][bs_expense_web_form][ADD] module initialized

---
+++ bs_expense_web_form/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Expense web form',
+    'description': """
+        Portal user will be able to submit his/her expenses through web form in website and a 
+        ===================================================
+        """,
+    'version': '15.0',
+    'author': "Brain Station 23 Ltd.",
+    'website': "http://brainstation-23.com",
+    'category': '',
+    'depends': ['website'],
+    'data': [
+    ],
+    'license': 'OPL-1',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
