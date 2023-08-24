f26184bc46142b2875293bd83a1bc75ae08079d1
Abu Raihan <abu.raihan@brainstation-23.com>
2023-08-25 01:16:17 +0600

    [EA-5754][bs_expense_web_form][ADD]:My expense page pagination, sort by, seacrh bar done

--- bs_expense_web_form/controller/web_form_controller.py
+++ bs_expense_web_form/controller/web_form_controller.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+from odoo.addons.portal.controllers.portal import pager
 
 
 class WebsiteForm(http.Controller):
@@ -39,14 +40,16 @@ class WebsiteForm(http.Controller):
         else:
             return request.redirect_query('/expense/submission/error')
 
-    @http.route('/my_expenses', type='http', auth='user', website=True)
-    def my_expenses_list(self):
+    @http.route(['/my_expenses', '/my_expenses/page/<int:page>'], type='http', auth='user', website=True)
+    def my_expenses_list(self, page=1, sortby='id', search="", search_in="All", **kwargs):
         expenses = request.env['hr.expense'].sudo().search([('employee_id.user_id', '=', request.env.user.id)])
+
         total_approved_amount = 0
         total_paid_amount = 0
         total_amount_to_submit = 0
         total_amount_submitted = 0
         total_amount_refused = 0
+
         for record in expenses:
             if record.state == 'draft':
                 total_amount_to_submit += record.total_amount
@@ -58,6 +61,34 @@ class WebsiteForm(http.Controller):
                 total_paid_amount += record.total_amount
             elif record.state == 'refused':
                 total_amount_refused += record.total_amount
+
+        """Get the Sorted List"""
+        sorted_expense_list = {
+            'id': {'label': 'Latest', 'order': 'id desc'},
+            'total_amount_company': {'label': 'Higher Amount', 'order': 'total_amount_company desc'},
+            'state': {'label': 'Status', 'order': 'state'}
+        }
+        default_order_by = sorted_expense_list[sortby]['order']
+
+        """Get Searched Items"""
+        search_expense_list = {
+            'All': {'label': 'All', 'input': 'All', 'domain': [('employee_id.user_id', '=', request.env.user.id)]},
+            'Name': {'label': 'Name', 'input': 'Name', 'domain': [('name', 'ilike', search), ('employee_id.user_id', '=', request.env.user.id)]},
+            'Status': {'label': 'Status', 'input': 'Status', 'domain': [('state', 'ilike', search), ('employee_id.user_id', '=', request.env.user.id)]}
+        }
+        search_domain = search_expense_list[search_in]['domain']
+
+        """Return records in Pagination"""
+        total_expense_count = expenses.search_count(search_domain)
+        page_details = pager(url='/my_expenses',
+                             total=total_expense_count,
+                             page=page,
+                             url_args={'sortby': sortby, 'search_in': search_in, 'search': search},
+                             step=10)
+        expenses = expenses.search(search_domain,
+                                   limit=10,
+                                   order=default_order_by,
+                                   offset=page_details['offset'], )
         values = {}
         values.update({'expenses': expenses,
                        'to_submit_amount': total_amount_to_submit,
@@ -65,5 +96,12 @@ class WebsiteForm(http.Controller):
                        'approved_amount': total_approved_amount,
                        'paid_amount': total_paid_amount,
                        'refused_amount': total_amount_refused,
+                       'page_name': "expense_page",
+                       'pager': page_details,
+                       'searchbar_sortings': sorted_expense_list,
+                       'sortby': sortby,
+                       'search_in': search_in,
+                       'searchbar_inputs': search_expense_list,
+                       'search': search
                        })
         return request.render("bs_expense_web_form.my_expenses", values)
